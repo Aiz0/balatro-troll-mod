728939f31dc810e1230065827219bc145f128199
Aiz <dev@aiz.moe>
2025-03-16 00:41:51 +0100

    feat: gros_michel

---
+++ jokers/gros_michel.lua
@@ -0,0 +1,33 @@
+return {
+    key = "gros_michel",
+    calculate = function(self, card, context)
+        if context.end_of_round and not context.individual and not context.repetition and not context.blueprint then
+            if #G.jokers.cards > 1 then
+                return { message = localize("k_safe_ex") }
+            else
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        play_sound("tarot1")
+                        card.T.r = -0.2
+                        card:juice_up(0.3, 0.4)
+                        card.states.drag.is = true
+                        card.children.center.pinch.x = true
+                        G.E_MANAGER:add_event(Event({
+                            trigger = "after",
+                            delay = 0.3,
+                            blockable = false,
+                            func = function()
+                                G.jokers:remove_card(card)
+                                card:remove()
+                                card = nil
+                                return true
+                            end,
+                        }))
+                        return true
+                    end,
+                }))
+                return { message = localize("k_extinct_ex") }
+            end
+        end
+    end,
+}
