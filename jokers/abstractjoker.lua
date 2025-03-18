d42205cfe7ead735a6188bcdd9e1dc950340e797
Sam <samstargazergustafsson@gmail.com>
2025-03-18 23:25:57 +0100

    okay is fixed

--- jokers/abstractjoker.lua
+++ jokers/abstractjoker.lua
@@ -1,20 +1,21 @@
-﻿return {
+﻿function getMult()
+    if G.hand and G.jokers and G.consumables then
+        return G.hand.config.card_limit + #G.jokers.cards + #G.consumables.cards;
+    end
+    return 0
+end
+
+return {
     key = "abstract",
-    config = {
-        extra = {
-            mult = 0,
-        }
-    },
     calculate = function(self, card, context)
-        card.ability.extra.mult = G.hand.config.card_limit + #G.jokers + #G.consumablees
-        return {
-            mult = card.ability.extra.mult
-        }
+        if context.joker_main then
+            return { mult = getMult() }
+        end
     end,
 
     -- localization
     loc_vars = function(self, info_queue, card)
-        return { vars = { card.ability.extra.mult } }
+        return { vars = { getMult() } }
     end,
     loc_txt = {
         name = "Abstract Joker",
@@ -22,7 +23,7 @@
             "{C:mult}+1{} Mult for",
             "each card outside",
             "your deck",
-            "({C:inactive}Currently {C:mult}+#1#{} Mult)",
+            "{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)",
         }
     },
 }
