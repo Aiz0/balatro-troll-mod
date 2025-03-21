3d11383d3336ce3b66388bcc499d6a6c9397dc10
Aiz <dev@aiz.moe>
2025-03-21 19:08:58 +0100

    feat: oops fixes shit when removed

--- jokers/oops.lua
+++ jokers/oops.lua
@@ -1,29 +1,90 @@
 return {
     key = "oops",
+    config = {
+        extra = {
+            joker_limit = 0,
+            consumables_limit = 0,
+            hand_size_offset = 0,
+            odds = 0,
+            playing_cards = {},
+            playing_card_ranks = {},
+            hands = 0,
+            discards = 0,
+            dollars = 0,
+            ante = 0,
+            round = 0,
+        },
+    },
     add_to_deck = function(self, card, from_debuff)
         -- set cardareas to 6
+        card.ability.extra.joker_limit = G.jokers.config.card_limit
         G.jokers.config.card_limit = 6
-        G.consumeables.config.card_limit = 6
-        G.hand:change_size(6 - G.hand.config.card_limit)
 
-        G.GAME.probabilities.normal = 6
+        card.ability.extra.consumables_limit = G.consumeables.config.card_limit
+        G.consumeables.config.card_limit = 6
+
+        card.ability.extra.hand_size_offset = 6 - G.hand.config.card_limit
+        G.hand:change_size(card.ability.extra.hand_size_offset)
+
+        if G.GAME.probabilities.normal > 0 then
+            card.ability.extra.odds = 6 / G.GAME.probabilities.normal
+            G.GAME.probabilities.normal = 6
+        end
 
         -- set cards to 6s
         for _, playing_card in ipairs(G.playing_cards) do
+            table.insert(card.ability.extra.playing_cards, playing_card)
+            table.insert(card.ability.extra.playing_card_ranks, playing_card.base.value)
             assert(SMODS.change_base(playing_card, nil, "6"))
         end
 
         -- hand
-        G.hand:change_size(6 - G.GAME.round_resets.hands)
+        card.ability.extra.hands = 6 - G.GAME.round_resets.hands
+        ease_hands_played(card.ability.extra.hands)
         G.GAME.round_resets.hands = 6
         -- discards
-        ease_discard(6 - G.GAME.round_resets.discards)
+        card.ability.extra.discards = 6 - G.GAME.round_resets.discards
+        ease_discard(card.ability.extra.discards)
         G.GAME.round_resets.discards = 6
         -- dollars
-        ease_dollars(6 - G.GAME.dollars)
+        card.ability.extra.dollars = 6 - G.GAME.dollars
+        ease_dollars(card.ability.extra.dollars)
 
         -- ante and round
-        ease_ante(6 - G.GAME.round_resets.ante)
-        ease_round(6 - G.GAME.round)
+        card.ability.extra.ante = 6 - G.GAME.round_resets.ante
+        ease_ante(card.ability.extra.ante)
+        card.ability.extra.round = 6 - G.GAME.round
+        ease_round(card.ability.extra.round)
+    end,
+
+    remove_from_deck = function(self, card, from_debuff)
+        -- set cardareas to 6
+        -- joker
+        G.jokers.config.card_limit = card.ability.extra.joker_limit - (6 - G.jokers.config.card_limit)
+        --consumables
+        G.consumeables.config.card_limit = card.ability.extra.consumables_limit - (6 - G.consumeables.config.card_limit)
+        -- hand size
+        G.hand:change_size(-card.ability.extra.hand_size_offset)
+
+        G.GAME.probabilities.normal = G.GAME.probabilities.normal / card.ability.extra.odds
+
+        -- set cards to back to what they were before
+        for i, playing_card in ipairs(card.ability.extra.playing_cards) do
+            assert(SMODS.change_base(playing_card, nil, card.ability.extra.playing_card_ranks[i]))
+        end
+        card.ability.extra.playing_cards = {}
+
+        -- hand
+        ease_hands_played(-card.ability.extra.hands)
+        G.GAME.round_resets.hands = G.GAME.round_resets.hands - card.ability.extra.hands
+        -- discards
+        ease_discard(-card.ability.extra.discards)
+        G.GAME.round_resets.discards = G.GAME.round_resets.discards - card.ability.extra.discards
+        -- dollars
+        ease_dollars(-card.ability.extra.dollars)
+
+        -- ante and round
+        ease_ante(-card.ability.extra.ante)
+        ease_round(-card.ability.extra.round)
     end,
 }
