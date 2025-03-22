d648c08a4c840ef15dfbe0c2b67b8802a156211a
Sam <samstargazergustafsson@gmail.com>
2025-03-22 15:40:01 +0100

    misprint lol

---
+++ jokers/misprint.lua
@@ -0,0 +1,13 @@
+﻿return {
+    key = "misprint",
+    calculate = function(self, card, context)
+        if
+        context.joker_main then
+            if pseudorandom(self.key) > 0.5 then
+                return { mult = 23 }
+            else
+                return { mult = 0 }
+            end
+        end
+    end,
+}
