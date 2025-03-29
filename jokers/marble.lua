9fefab4d0d0bc4548b7159b804402cb581b1c992
Aiz <dev@aiz.moe>
2025-03-30 00:43:09 +0100

    fix(marble): getting sliced check

--- jokers/marble.lua
+++ jokers/marble.lua
@@ -7,6 +7,6 @@ local mod_prefix = SMODS.current_mod.prefix
 return {
     key = "marble",
     calculate = function(self, card, context)
-        if context.setting_blind then play_sound(mod_prefix .. "_marbles", nil, 0.1) end
+        if context.setting_blind and not card.getting_sliced then play_sound(mod_prefix .. "_marbles", nil, 0.1) end
     end,
 }
