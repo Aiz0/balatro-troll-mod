bf68a91e72051f23636c6cc361cf6f3afa70c64c
Aiz <dev@aiz.moe>
2025-04-01 23:33:18 +0200

    feat: drivers license

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -3,6 +3,9 @@ return {
         dictionary = {
             k_folly_shoot_the_moon = "Shoot the moon!",
             k_folly_mega_jackpot = "Mega Jackpot!",
+            k_folly_drivers_license_expired = "Expired!",
+            k_folly_renew_drivers_license = "Renew",
+            k_folly_drivers_license_renewed = "Renewed!",
         },
     },
 }
