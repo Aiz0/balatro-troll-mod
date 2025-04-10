237ee6e50fc9acef2141b51a73977bbfc5bc8c60
Stargazer <54099294+NotStargazer@users.noreply.github.com>
2025-04-10 21:06:55 +0200

    Update utils.lua
    Co-authored-by: Aiz <66974576+Aiz0@users.noreply.github.com>

--- utils.lua
+++ utils.lua
@@ -31,32 +31,4 @@ folly_utils = {
         joker = "j_" .. SMODS.current_mod.prefix .. "_"
     },
 
-    stack = function()
-        return setmetatable({
-            -- stack table  
-            _stack = {},
-            -- size of stack
-            count = 0,
-
-            -- push an element to the stack underlying array
-            push = function(self, obj)
-                -- increment the index
-                self.count = self.count + 1
-                -- set the element at the end of the array
-                rawset(self._stack, self.count, obj)
-            end,
-
-            -- pop an element from the stack
-            pop = function(self)
-                -- decrement the index    
-                self.count = self.count - 1
-                -- remove and return the last element
-                return table.remove(self._stack)
-            end,
-        }, {
-            __index = function(self, index)
-                return rawget(self._stack, index)
-            end,
-        })
-    end
 }
