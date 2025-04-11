d2c7123c239d8a4a2ba3a9fe92bd409c2b54343e
Aiz <dev@aiz.moe>
2025-04-11 23:32:40 +0200

    feat: greedy joker

---
+++ jokers/greedy_joker.lua
@@ -0,0 +1,11 @@
+return {
+    key = "greedy_joker",
+    calculate = function(self, card, context)
+        if context.end_of_round and not context.individual and not context.repetition then
+            return {
+                message = localize("k_folly_greed"),
+                func = function() ease_dollars(-1) end,
+            }
+        end
+    end,
+}
