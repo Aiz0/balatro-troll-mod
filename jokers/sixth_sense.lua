a15d9fbf2aee058969022ed5f158491c10369475
Sam <samstargazergustafsson@gmail.com>
2025-04-22 00:45:35 +0200

    okay everything fixed

--- jokers/sixth_sense.lua
+++ jokers/sixth_sense.lua
@@ -8,6 +8,12 @@ SMODS.Consumable({
     key = "six",
     set = "funny",
     config = {
+        extra = {
+            edition,
+            suit,
+            seal,
+            enhancement,
+        },
         max_highlighted = 2,
     },
     no_collection = true,
@@ -23,10 +29,10 @@ SMODS.Consumable({
         end
         for i, v in pairs(G.hand.highlighted) do
             G.E_MANAGER:add_event(Event({ trigger = 'after', delay = 0.5, func = function()
-                v:set_edition(card.ability.edition)
-                v:set_seal(card.ability.seal)
-                v:set_ability(card.ability.enhancement)
-                SMODS.change_base(v, card.ability.suit, '6')
+                v:set_edition(card.ability.extra.edition)
+                v:set_seal(card.ability.extra.seal)
+                v:set_ability(card.ability.extra.enhancement)
+                SMODS.change_base(v, card.ability.extra.suit, '6')
                 return true
             end }))
         end
@@ -47,16 +53,20 @@ SMODS.Consumable({
     in_pool = function(self, args)
         return false
     end,
-    load = function(self, card, card_table, other_card)
-        card:set_edition(card.ability.edition)
-        card:set_seal(card.ability.seal)
-        SMODS.change_base(card, card.ability.suit, '6')
-    end,
     set_sprites = function(self, card, front)
         if card.ability then
-            card:set_sprites(G.P_CENTERS[card.ability.enhancement])
+            if card.ability.enhancement then
+                card:set_sprites(G.P_CENTERS[card.ability.enhancement])
+            elseif card.ability.extra then
+                card:set_sprites(G.P_CENTERS[card.ability.extra.enhancement])
+            end
         end
     end,
+    add_to_deck = function(self, card, from_debuff)
+        if not from_debuff then
+            card:set_sprites(G.P_CENTERS[card.ability.extra.enhancement])
+        end
+    end
 })
 
 function random_suit()
@@ -87,19 +97,19 @@ return {
                     trigger = 'before',
                     func = function()
                         local area = G.consumeables
-                        local create_card = SMODS.create_card({
+                        local created_card = SMODS.create_card({
                             key = 'c_folly_six'
                         })
                         local edition_rate = 2
                         local edition = poll_edition('standard_edition' .. G.GAME.round_resets.ante, edition_rate, true)
                         local suit = random_suit()
-                        create_card.ability.suit = suit
-                        SMODS.change_base(create_card, random_suit(), '6')
-                        create_card:set_edition(edition, true, true)
-                        create_card.ability.edition = edition
+                        created_card.ability.extra.suit = suit
+                        SMODS.change_base(created_card, suit, '6')
+                        created_card:set_edition(edition, true, true)
+                        created_card.ability.extra.edition = edition
                         local seal = SMODS.poll_seal({ mod = 10 })
-                        create_card:set_seal(seal)
-                        create_card.ability.seal = seal
+                        created_card:set_seal(seal)
+                        created_card.ability.extra.seal = seal
                         local enhancement = SMODS.poll_enhancement({ mod = 6, options = {
                             "m_bonus",
                             "m_mult",
@@ -109,10 +119,11 @@ return {
                             "m_gold",
                             "m_lucky",
                         } })
-                        create_card.ability.enhancement = enhancement
-                        create_card:set_sprites(G.P_CENTERS[enhancement])
-                        area:emplace(create_card)
+                        created_card.ability.extra.enhancement = enhancement
+                        created_card:set_sprites(G.P_CENTERS[enhancement])
+                        area:emplace(created_card)
                         G.GAME.consumeable_buffer = 0
+                        created_card:set_debuff(false)
                         return true
                     end,
                 }))
