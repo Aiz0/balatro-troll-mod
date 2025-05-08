2bcbeeaba69ee106526b6d24df523330cea9d859
Sam <samstargazergustafsson@gmail.com>
2025-05-08 20:20:34 +0200

    review fixes

--- jokers/astronomer.lua
+++ jokers/astronomer.lua
@@ -5,8 +5,9 @@ return {
             astro = nil
         },
     },
-    astronomers = 0,
     add_to_deck = function(self, card, from_debuff)
-        card.ability.extra.astro = pseudorandom_element(G.P_CENTER_POOLS.Planet, pseudoseed(self.key)).key
+        if not from_debuff then
+            card.ability.extra.astro = pseudorandom_element(G.P_CENTER_POOLS.Planet, pseudoseed(self.key)).key
+        end
     end,
 }
