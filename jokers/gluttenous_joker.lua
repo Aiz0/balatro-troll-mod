81d938b6fb7dd9ced4ec031eacae91371a178dad
Aiz <dev@aiz.moe>
2025-05-09 15:06:38 +0200

    feat: add munch sound

--- jokers/gluttenous_joker.lua
+++ jokers/gluttenous_joker.lua
@@ -1,9 +1,17 @@
+SMODS.Sound({
+    key = "munch",
+    path = "munch.ogg",
+})
 return {
     key = "gluttenous_joker",
     calculate = function(self, card, context)
         if context.destroy_card and context.cardarea == G.play and context.destroy_card:is_suit("Clubs") then
             if pseudorandom(self.key) < G.GAME.probabilities.normal / 6 then
-                return { remove = true, message = localize("k_folly_gluttenous") }
+                return {
+                    remove = true,
+                    message = localize("k_folly_gluttenous"),
+                    sound = "folly_munch",
+                }
             end
         end
     end,
