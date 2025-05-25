a0dbaca1aca838cda83b21291d40ff129b1c9c56
Sam <samstargazergustafsson@gmail.com>
2025-05-25 02:52:10 +0200

    1 gazillion

---
+++ jokers/golden.lua
@@ -0,0 +1,20 @@
+return {
+    key = "golden",
+    cost = 0,
+    set_ability = function(self, card, initial, delay_sprites)
+        card.cost = G.GAME.dollars + 1
+    end,
+    dollars_updated = function(self, card, change)
+        card.cost = G.GAME.dollars + 1
+    end,
+    loc_vars = function(self, info_queue, card)
+        if card.area == G.jokers then
+            return { vars = {
+                0,
+            } }
+        end
+        return { vars = {
+                localize("k_folly_golden")
+            } }
+    end,
+}
