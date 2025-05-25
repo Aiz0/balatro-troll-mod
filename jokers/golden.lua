ea371d35bd05b15125d111ad124224b538585412
Sam <samstargazergustafsson@gmail.com>
2025-05-25 23:28:49 +0200

    full functional now

--- jokers/golden.lua
+++ jokers/golden.lua
@@ -2,13 +2,19 @@ return {
     key = "golden",
     cost = 1,
     set_ability = function(self, card, initial, delay_sprites)
-        card.base_cost = G.GAME.dollars + 1
+        folly_utils.log.Info(G.GAME.discount_percent)
+        local discount = 1 - G.GAME.discount_percent / 100
+        card.base_cost = (G.GAME.dollars + 1) / discount
     end,
     calculate_dollar_bonus = function(self, card)
         return 0
     end,
     dollars_updated = function(self, card, change)
-        card.cost = G.GAME.dollars + 1
+        local discount = 1 - G.GAME.discount_percent / 100
+        card.base_cost = (G.GAME.dollars + 1) / discount
+        if card.area == G.jokers then
+            card:set_cost()
+        end
     end,
     loc_vars = function(self, info_queue, card)
         if card.area == G.jokers then
