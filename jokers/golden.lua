72ac4f22d7804eb78bf61e7d780283c5e1c9e2ec
Stargazer <54099294+NotStargazer@users.noreply.github.com>
2025-05-30 23:19:14 +0200

    Update jokers/golden.lua
    Co-authored-by: Aiz <66974576+Aiz0@users.noreply.github.com>

--- jokers/golden.lua
+++ jokers/golden.lua
@@ -2,7 +2,6 @@ return {
     key = "golden",
     cost = 1,
     set_ability = function(self, card, initial, delay_sprites)
-        folly_utils.log.Info(G.GAME.discount_percent)
         local discount = 1 - G.GAME.discount_percent / 100
         card.base_cost = (G.GAME.dollars + 1) / discount
     end,
